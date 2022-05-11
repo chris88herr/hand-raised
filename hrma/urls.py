7a6057da7d017f8dbc865526ae287a24332ec181
chris88herr <chris88herrera@gmail.com>
2022-05-10 20:15:56 -0700

    added course regex pattern for student's workflow

--- hrma/urls.py
+++ hrma/urls.py
@@ -6,6 +6,7 @@ app_name = 'hrma'
 urlpatterns = [
     path('professorDashboard', views.ProfessorDashboard.as_view(), name='professorDashboard'),
     path('studentDashboard', views.StudentDasboard.as_view(), name='studentDashboard'),
+    path('studentDashboard/courses/<str:course_id>/', views.CoursesView.as_view(), name='professorDashboard'),
     path('accounts/login/', views.login, name='login'),
     path('signup', views.SignUpView.as_view(), name='signup'),
     path('home', views.home, name='home'),
